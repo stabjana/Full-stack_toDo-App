3dac60ae08e93c65152410e53c573dfb77ab76c1
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-05-22 21:12:10 +0300

    unknown changes from some time ago

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,36 @@
-import express, { Request, Response, NextFunction } from 'express';
-import todoRoutes from './routes/todo';
-import { json, urlencoded } from 'body-parser';
-import db from 'mongoose';
-import cors from 'cors';
+import express, { Request, Response, NextFunction } from "express";
+import todoRoutes from "./routes/todo";
+import { json, urlencoded } from "body-parser";
+import db from "mongoose";
+import cors from "cors";
 
 const app = express();
-app.use(  cors({    origin: 'http://localhost:5173'  }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors());
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-app.listen(3001);
-app.use('/todos', todoRoutes);
+app.use("/todos", todoRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("Fehler:", err.message);
   res.status(500).json({ message: err.message });
 });
 
-db.connect('mongodb://127.0.0.1:27017/todos');
+/* app.listen(3001, () => console.log("Server läuft auf Port 3001"));
+db.connect("mongodb://127.0.0.1:27017/todos"); */
+
+db.connect("mongodb://127.0.0.1:27017/todos")
+  .then(() => {
+    console.log("MongoDB verbunden!");
+    app.listen(3001, () => console.log("Server läuft auf Port 3001"));
+  })
+  .catch((err) => console.error("MongoDB Verbindungsfehler:", err));
--- backend/src/controllers/todos.ts
+++ backend/src/controllers/todos.ts
@@ -1,18 +1,16 @@
-import { RequestHandler } from 'express';
-import Todo, { TodoModel }  from '../models/todo';
-import { get, request } from 'http';
+import { RequestHandler } from "express";
+import Todo, { TodoModel } from "../models/todo";
+import { get, request } from "http";
 
 /* const TODOS: Todo[] = []; */
 export const createTodo: RequestHandler = async (req, res, next) => {
   try {
     const data: TodoModel = req.body;
-    console.log('Data', data);
+    console.log("Data", data);
     let todos = await Todo.create(data);
-     res
-      .status(200)
-      .json({ message: 'Todo created sucessfully', data: todos });
+    res.status(200).json({ todos });
   } catch (error: any) {
-     res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -30,8 +28,8 @@ export const updateTodo: RequestHandler = async (req, res, next) => {
     const todoId = req.params.id;
     const data: TodoModel = req.body;
     let updatedToDO = await Todo.findByIdAndUpdate(todoId, data, { new: true });
-      res.status(200).json(updatedToDO);
-  } catch (error:any) {
+    res.status(200).json(updatedToDO);
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 };
@@ -40,7 +38,7 @@ export const deleteTodo: RequestHandler = async (req, res, next) => {
   try {
     const todoId = req.params.id;
     let todos = await Todo.findByIdAndDelete(todoId);
-    res.status(200).json({ message: 'Todo deleted!' });
+    res.status(200).json({ message: "Todo deleted!" });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
